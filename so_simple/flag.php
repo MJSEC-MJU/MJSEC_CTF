9cee5db7581f2465400326e12fef46f17310c9a5
Jongcoding <ialleejy@gmail.com>
2024-12-17 08:33:04 +0900

    update:so_simple

---
+++ so_simple/flag.php
@@ -0,0 +1,23 @@
+<html><head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        <title>flag page</title>
+    </head>
+    <body>
+    <div><h1>flag.php!</h1>
+        <p></p><h3>flag is MJSEC2024{fake_flag}</h3><p></p>
+        <br><h3>PATH:  <br><br></h3></div>
+     <hr>        
+    <style type="text/css">
+        body {
+            margin: 1em;
+        }
+        div {
+            margin: 0 5px 0 0;
+            padding: 0.1em;
+            border: 2px solid silver;
+            border-radius: 7px;
+        }
+
+    </style>
+    
+</body></html>
